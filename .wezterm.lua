1c618a6f818a4ed0f9690aac68019e02e63fbb63
ishiooon <ishiooon140@gmail.com>
2025-03-14 15:43:47 +0900

    .wezterm.luaのテスト中

--- .wezterm.lua
+++ .wezterm.lua
@@ -2,15 +2,19 @@
 local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
-local config = wezterm.config_builder()
+local config = {}
+
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
 
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = 'Snazzy'
+config.color_scheme = 'MaterialDesignColors'
 
 -- Changing the default program: wsl
 config.default_prog = { "wsl.exe", "--distribution", "ubuntu", "--cd", "~" }
 
 -- and finally, return the configuration to wezterm
-return config"~"
+return config
