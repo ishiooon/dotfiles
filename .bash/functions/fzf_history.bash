f9ae78889b03715ea5db7dba73bc3c5a218fac69
ishio <ishio@itsj.jp>
2025-04-15 10:49:48 +0900

    bashのhfのエラーを修正

--- .bash/functions/fzf_history.bash
+++ .bash/functions/fzf_history.bash
@@ -37,8 +37,8 @@ hf() {
     --header-first \
     --query="" \
     --color=bg+:#3B4252,bg:#2E3440,spinner:#81A1C1,hl:#88C0D0,fg:#D8DEE9,header:#616E88,info:#81A1C1,pointer:#81A1C1,marker:#A3BE8C,fg+:#D8DEE9,prompt:#81A1C1,hl+:#88C0D0 \
-    --bind "ctrl-d:execute-silent(source ~/.bash/functions/history_management.bash && delete_from_history {}) + execute-silent(echo -e '\033[1;31m履歴から削除: {}\033[0m' >&2) + reload(cat \"$history_temp_file\" | awk '!seen[$0]++')"\
-    --bind "ctrl-r:execute-silent(echo -e '\033[1;32m履歴を更新しました\033[0m' >&2)+reload(refresh_history_file)" \
+    --bind "ctrl-d:execute(source ~/.bash/functions/history_management.bash && delete_from_history {} > /dev/null 2>&1)+execute(echo -e '\033[1;31m履歴から削除: {}\033[0m' >&2)+reload(cat \"$history_temp_file\" | awk '!seen[$0]++')" \
+    --bind "ctrl-r:execute(echo -e '\033[1;32m履歴を更新しました\033[0m' >&2)+reload(refresh_history_file)" \
     --expect=tab,enter)
   
   # fzfの出力から押されたキーと選択されたコマンドを取得
