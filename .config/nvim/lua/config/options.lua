715af1b93b36a15366db0d185936604c55ab7cc1
ishio <ishio@itsj.jp>
2025-04-21 14:07:06 +0900

    各種エラーの修正など

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -75,15 +75,22 @@ local mason = require('mason')
 local lspconfig = require('lspconfig')
 local mason_lspconfig = require('mason-lspconfig')
 
+-- lsp自動補完設定
+-- lspのハンドラーに設定（グローバル変数ではなくローカル変数に変更）
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 mason.setup()
 mason_lspconfig.setup()
 mason_lspconfig.setup_handlers({
     function(server_name)
-        lspconfig[server_name].setup({})
+        lspconfig[server_name].setup({
+            capabilities = capabilities
+        })
     end,
     -- laravelの「Route」で「undefinedTypes」エラーが出るため Intelephenseのdiagnostics.undefinedTypesをfalseにする
     ["intelephense"] = function()
         lspconfig.intelephense.setup({
+            capabilities = capabilities,
             settings = {
                 intelephense = {
                     diagnostics = {
@@ -95,9 +102,6 @@ mason_lspconfig.setup_handlers({
     end
 })
 
--- lsp自動補完設定
--- lspのハンドラーに設定
-capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 -- lspの設定後に追加
 vim.opt.completeopt = "menu,menuone,noselect"
