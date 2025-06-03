be8c7369f1dfa88fee0db6e77c8d363d9daf43ec
ishio <ishio@itsj.jp>
2025-06-03 10:50:11 +0900

    履歴の設定を変更

--- .bash/history.bash
+++ .bash/history.bash
@@ -1,5 +1,11 @@
 # 履歴関連の設定
 
+# bash履歴機能を有効にする
+set -o history
+
+# 履歴ファイルの場所を指定
+HISTFILE=~/.bash_history
+
 # 履歴に重複行やスペースで始まる行を入れない
 HISTCONTROL=ignoreboth
 
@@ -9,3 +15,6 @@ shopt -s histappend
 # 履歴の長さの設定
 HISTSIZE=10000
 HISTFILESIZE=20000
+
+# 各コマンド実行後に履歴をファイルに保存
+PROMPT_COMMAND="history -a; $PROMPT_COMMAND"
