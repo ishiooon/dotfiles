66ea1b33b7e215e0f3d22a259584ca93af6b14b0
ishio <ishio@itsj.jp>
2025-06-13 11:48:04 +0900

    claude-codeプラグインのついか、dotfilesの追加

--- .config/nvim/lua/plugins/claude-code.lua
+++ .config/nvim/lua/plugins/claude-code.lua
@@ -8,8 +8,8 @@ return {
   config = function()
     require("claude-code").setup({
        window = {
-        split_ratio = 0.2,      -- Percentage of screen for the terminal window (height for horizontal, width for vertical splits)
-        position = "rightbelow vsplit",  -- Position of the window: "botright", "topleft", "vertical", "rightbelow vsplit", etc.
+        -- split_ratio = 0.2,      -- Percentage of screen for the terminal window (height for horizontal, width for vertical splits)
+        -- position = "rightbelow vsplit",  -- Position of the window: "botright", "topleft", "vertical", "rightbelow vsplit", etc.
       }
     })
   end
