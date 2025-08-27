8ec0a9a7d48513937a86c711dfeb3b6e9bac7a07
ishio <ishio@itsj.jp>
2025-08-27 17:19:52 +0900

    claudecode-lualine.nvimをgitから直接ロード

--- .config/nvim/lua/plugins/claudecode-lualine.lua
+++ .config/nvim/lua/plugins/claudecode-lualine.lua
@@ -1,24 +1,10 @@
 -- Claude Code status component for lualine
 return {
   {
-    dir = vim.fn.expand('~/dev_plugin/claudecode-lualine.nvim'),
+    'ishiooon/claudecode-lualine.nvim',
     dependencies = {
       'nvim-lualine/lualine.nvim',
       'coder/claudecode.nvim',
     },
-    config = function()
-      -- ローカルディレクトリが存在しない場合は GitHubから取得
-      local plugin_dir = vim.fn.expand('~/dev_plugin/claudecode-lualine.nvim')
-      if vim.fn.isdirectory(plugin_dir) == 0 then
-        vim.notify('claudecode-lualine.nvim not found locally, cloning from GitHub...', vim.log.levels.INFO)
-        vim.fn.system({
-          'git', 'clone', 'https://github.com/ishiooon/claudecode-lualine.nvim',
-          plugin_dir
-        })
-      end
-      
-      -- The component will be added to lualine in the lualine.lua config
-      -- This just ensures the plugin is loaded
-    end,
   },
 }
