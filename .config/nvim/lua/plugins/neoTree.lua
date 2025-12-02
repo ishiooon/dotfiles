355d49e2cdfdb32ac7fc09fa8be2d397deb2fccf
ishiooon <ishiooon@gmail.com>
2025-12-02 10:30:35 +0900

    neoTreeの表示を修正

--- .config/nvim/lua/plugins/neoTree.lua
+++ .config/nvim/lua/plugins/neoTree.lua
@@ -8,7 +8,18 @@ return{
           -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
         },
         opts =  {
-            position =  'current',
+            position =  "current",
+            sources = { "filesystem", "git_status", "buffers" }, -- Git差分のみ表示するためにgit_statusソースを有効化
+            source_selector = {
+                winbar = true,
+                content_layout = "center",
+                sources = {
+                    { source = "filesystem", display_name = "Files" },
+                    { source = "git_status", display_name = "Git" }, -- Git変更のみをツリーに絞り込むフィルタ
+                    { source = "buffers", display_name = "Buffers" },
+                },
+            },
+            default_source = "filesystem",
             event_handlers = {
                 {
                   event = "neo_tree_buffer_enter",
